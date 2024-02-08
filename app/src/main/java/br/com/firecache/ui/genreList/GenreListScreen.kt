b8bc4e6cc29488b7589963f72b56d76770ba5187
matheusperezz <matheus.mperez08@gmail.com>
2024-02-08 12:27:07 -0300

    add: listing genres in bottom genre

--- app/src/main/java/br/com/firecache/ui/genreList/GenreListScreen.kt
+++ app/src/main/java/br/com/firecache/ui/genreList/GenreListScreen.kt
@@ -1,24 +1,58 @@
 package br.com.firecache.ui.genreList
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import br.com.firecache.ui.navigation.FirecacheFab
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun GenreListScreen(
-    onAddGenreClick: () -> Unit = {}
+    onAddGenreClick: () -> Unit = {},
+    viewModel: GenreListViewModel = hiltViewModel()
 ) {
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
     Scaffold(
         floatingActionButton = {
             FirecacheFab(text = "Adicionar gênero", onClick = onAddGenreClick)
         }
     ) {
-        Box(modifier = Modifier.padding(it)) {
-            Text(text = "Genres")
+        when(uiState){
+            is GenreListUiState.Loading -> {
+                Box(modifier = Modifier.padding(16.dp)) {
+                    Text("Carregando...")
+                }
+            }
+            is GenreListUiState.Success -> {
+                val genres = (uiState as GenreListUiState.Success).genres
+                LazyColumn {
+                    items(genres){ genre ->
+                        Text(text = genre.name, modifier = Modifier.padding(16.dp))
+                    }
+                }
+            }
+            is GenreListUiState.Error -> {
+                Box(modifier = Modifier.padding(16.dp)) {
+                    Text("Erro ao carregar os gêneros")
+                }
+            }
+            is GenreListUiState.Empty -> {
+                Box(modifier = Modifier.padding(16.dp)) {
+                    Text("Nenhum gênero encontrado")
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/br/com/firecache/ui/genreList/GenreListViewModel.kt
@@ -0,0 +1,55 @@
+package br.com.firecache.ui.genreList
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.firecache.data.models.BookCardModel
+import br.com.firecache.data.models.Genre
+import br.com.firecache.data.repositories.GenreRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+sealed class GenreListUiState {
+    object Loading : GenreListUiState()
+    data class Success(val genres: List<Genre>) : GenreListUiState()
+    data class Error(val error: Throwable) : GenreListUiState()
+    object Empty : GenreListUiState()
+}
+
+@HiltViewModel
+class GenreListViewModel @Inject constructor(
+    private val genreRepository: GenreRepository
+): ViewModel(){
+
+    private var currentUiStateJob: Job? = null
+    private val _uiState = MutableStateFlow<GenreListUiState>(
+        GenreListUiState.Loading
+    )
+    val uiState = _uiState
+
+    init {
+        fetchGenres()
+    }
+
+    private fun fetchGenres(){
+        currentUiStateJob?.cancel()
+        currentUiStateJob = viewModelScope.launch {
+            _uiState.value = GenreListUiState.Loading
+            try {
+                genreRepository.fetchAllGenres().collect {
+                    if (it.isEmpty()) {
+                        _uiState.value = GenreListUiState.Empty
+                    } else {
+                        _uiState.value = GenreListUiState.Success(it)
+                    }
+                }
+            } catch (e: Throwable) {
+                _uiState.value = GenreListUiState.Error(e)
+            }
+        }
+    }
+}
